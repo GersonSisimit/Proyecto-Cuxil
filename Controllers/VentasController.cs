4bd6334e44fb25a2cf015110fb6491e845450ed6
Gerson Sisimit Sanai <gersonsisimit@gmail.com>
2023-12-17 10:05:21 -0600

    Controlador de proceso de ventas

---
+++ Controllers/VentasController.cs
@@ -0,0 +1,23 @@
+﻿using AgroservicioCuxil.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgroservicioCuxil.Controllers
+{
+    public class VentasController : Controller
+    {
+        private readonly AgroservicioContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public VentasController(AgroservicioContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Venta()
+        {
+            return View();
+        }
+    }
+}
